85e5d9c7edf4a6b1829e2ebe3f2e5402ae5e46d6
zhwatts <zach@zachwatts.online>
2025-02-01 17:21:22 -0500

    make backend work on vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,10 +3,20 @@
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as express from "express";
+import serverlessExpress from "@vendia/serverless-express";
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp)
+  );
+  app.enableCors(); // Enable CORS for API access
 
+  // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle("Placedly API")
     .setDescription("The Placedly API description")
@@ -22,6 +32,11 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.init(); // Initialize NestJS app within Express
 }
+
+// Bootstrap the NestJS app
 bootstrap();
+
+// Export the handler for Vercel's Serverless Functions
+export const handler = serverlessExpress({ app: expressApp });
