74ce181949c0a9dfbc94d1e7a431cbe5b8f06fc7
zhwatts <zach@zachwatts.online>
2025-02-01 22:58:27 -0500

    updates to config for vercel frontend dep

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,12 @@
-/** @format */
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
   server: {
     proxy: {
       "/api": {
@@ -14,9 +15,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
   },
 });
